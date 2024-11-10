3e4395cd9e3cd574b39c51c17524eceed571b6a7
mariesavch <mariesavch@icloud.com>
2024-11-10 12:36:02 +0500

    css in rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 #![allow(non_snake_case)]
 
 use async_std::task::sleep;
+use css_in_rs::{make_styles, use_style_provider_quickstart, Classes, EmptyTheme};
 use dioxus::prelude::*;
 use serde::Deserialize;
-use sir::{css, global_css, AppStyle};
 use web_time::Instant;
 
 fn main() {
@@ -32,8 +32,116 @@ async fn get_weather() -> reqwest::Result<WeatherData> {
         .await
 }
 
+make_styles! {
+    (_theme: EmptyTheme) -> MyClasses {
+        ":root" {
+            __rosewater: "#ff8389",
+            __flamingo: "#ff8389",
+            __red: "#ff8389",
+            __maroon: "#ff8389",
+            __pink: "#ff7eb6",
+            __mauve: "#be95ff",
+            __peach: "#d44a1c",
+            __yellow: "#ab8600",
+            __green: "#08bdba",
+            __teal: "#33b1ff",
+            __sky: "#33b1ff",
+            __sapphire: "#33b1ff",
+            __blue: "#78a9ff",
+            __lavender: "#78a9ff",
+            __text: "#ffffff",
+            __subtext1: "#f4f4f4",
+            __subtext0: "#e0e0e0",
+            __overlay2: "#adadad",
+            __overlay1: "#949494",
+            __overlay0: "#7a7a7a",
+            __surface2: "#4f4f4f",
+            __surface1: "#383838",
+            __surface0: "#2e2e2e",
+            __base: "#161616",
+            __mantle: "#0d0d0d",
+            __crust: "#000000",
+        },
+    "@media (prefers-color-scheme: light)" {
+        ":root" {
+            __rosewater: "#da1e28",
+            __flamingo: "#da1e28",
+            __red: "#da1e28",
+            __maroon: "#da1e28",
+            __pink: "#d02670",
+            __mauve: "#8a3ffc",
+            __peach: "#d44a1c",
+            __yellow: "#ab8600",
+            __green: "#007d79",
+            __teal: "#1192e8",
+            __sky: "#1192e8",
+            __sapphire: "#1192e8",
+            __blue: "#0f62fe",
+            __lavender: "#0f62fe",
+            __text: "#000000",
+            __subtext1: "#404040",
+            __subtext0: "#474747",
+            __overlay2: "#575757",
+            __overlay1: "#595959",
+            __overlay0: "#737373",
+            __surface2: "#8c8c8c",
+            __surface1: "#d1d1d1",
+            __surface0: "#e6e6e6",
+            __base: "#ffffff",
+            __mantle: "#f2f2f2",
+            __crust: "#ebebeb",
+            }
+        },
+        ":root" {
+            background_color: "var(--base)",
+            color: "var(--text)",
+            line_height: "1.6",
+        },
+        "@media (hover: hover) and (pointer: fine)" {
+            ".animated_list li" {
+                all: "unset",
+                transition_property: "all",
+                transition_timing_function: "cubic-bezier(0.4, 0, 0.2, 1)",
+                transition_duration: "300ms",
+            },
+            ".animated_list:hover li" {
+                opacity: "0.5",
+            },
+            ".animated_list:hover li:hover" {
+                opacity: "1",
+            }
+        },
+        ".item" {
+            display: "flex",
+            flex_direction: "column",
+            padding_top: "12px",
+            padding_bottom: "12px",
+            gap: "4px",
+        },
+        ".underlined" {
+            color: "unset",
+            text_decoration_line: "underline",
+            text_decoration_thickness: "2px",
+            text_underline_offset: "4px",
+            transition_property: "color, background-color, border-color, text-decoration-color, fill, stroke",
+            transition_timing_function: "cubic-bezier(0.4, 0, 0.2, 1)",
+            transition_duration: "300ms",
+            text_decoration_color: "var(--surface2)",
+        },
+        ".underlined:hover" {
+            text_decoration_color: "var(--overlay2)",
+        },
+        ".underlined:active" {
+            text_decoration_color: "var(--overlay1)",
+        }
+    }
+}
+
 #[component]
 fn App() -> Element {
+    use_style_provider_quickstart(|| EmptyTheme);
+    let cls: &MyClasses = &MyClasses::use_style();
+
     let weather = use_resource(move || async move { get_weather().await });
     let mut millis = use_signal(|| 0);
 
@@ -52,126 +160,7 @@ fn App() -> Element {
         millis() % 1000
     );
 
-    global_css!(
-        "
-       :root {
-        --rosewater: #ff8389;
-        --flamingo: #ff8389;
-        --red: #ff8389;
-        --maroon: #ff8389;
-        --pink: #ff7eb6;
-        --mauve: #be95ff;
-        --peach: #d44a1c;
-        --yellow: #ab8600;
-        --green: #08bdba;
-        --teal: #33b1ff;
-        --sky: #33b1ff;
-        --sapphire: #33b1ff;
-        --blue: #78a9ff;
-        --lavender: #78a9ff;
-        --text: #ffffff;
-        --subtext1: #f4f4f4;
-        --subtext0: #e0e0e0;
-        --overlay2: #adadad;
-        --overlay1: #949494;
-        --overlay0: #7a7a7a;
-        --surface2: #4f4f4f;
-        --surface1: #383838;
-        --surface0: #2e2e2e;
-        --base: #161616;
-        --mantle: #0d0d0d;
-        --crust: #000000;
-    } 
-
-    @media (prefers-color-scheme: light) {
-        :root {
-            --rosewater: #da1e28;
-            --flamingo: #da1e28;
-            --red: #da1e28;
-            --maroon: #da1e28;
-            --pink: #d02670;
-            --mauve: #8a3ffc;
-            --peach: #d44a1c;
-            --yellow: #ab8600;
-            --green: #007d79;
-            --teal: #1192e8;
-            --sky: #1192e8;
-            --sapphire: #1192e8;
-            --blue: #0f62fe;
-            --lavender: #0f62fe;
-            --text: #000000;
-            --subtext1: #404040;
-            --subtext0: #474747;
-            --overlay2: #575757;
-            --overlay1: #595959;
-            --overlay0: #737373;
-            --surface2: #8c8c8c;
-            --surface1: #d1d1d1;
-            --surface0: #e6e6e6;
-            --base: #ffffff;
-            --mantle: #f2f2f2;
-            --crust: #ebebeb;
-        }
-    }
-
-    :root {
-        background-color: var(--base);
-        color: var(--text);
-        line-height: 1.6;
-    }
-
-    "
-    );
-
-    let underlined =css!("
-    color: unset;
-    text-decoration-line: underline; 
-    text-decoration-thickness: 2px; 
-    text-underline-offset: 4px; 
-    transition-property: color, background-color, border-color, text-decoration-color, fill, stroke; 
-    transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1); 
-    transition-duration: 300ms;
-    text-decoration-color: var(--surface2);
-
-    &:hover {
-        text-decoration-color: var(--overlay2);
-    }
-
-    &:active {
-        text-decoration-color: var(--overlay1);
-    }
-    ");
-
-    let animated_list = css!(
-        "
-    @media (hover: hover) and (pointer: fine) {
-        li {
-            all: unset;
-            transition-property: all;
-            transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-            transition-duration: 300ms;
-        }
-        &:hover li {
-            opacity: 0.5;
-        }
-        &:hover li:hover {
-            opacity: 1;
-        }
-    }
-    "
-    );
-
-    let item = css!(
-        "
-        display: flex;
-        flex-direction: column;
-        padding-top: 12px;
-        padding-bottom: 12px;
-        gap: 4px;"
-    );
-
     rsx! {
-        AppStyle {}
         main {
             margin_left: "auto",
             margin_right: "auto",
@@ -191,15 +180,15 @@ fn App() -> Element {
                 }
             }
             ul {
-                class: animated_list,
+                class: &cls.animated_list as &str,
                 all: "unset",
                 display: "grid",
                 grid_template_columns: "repeat(auto-fit, minmax(300px, 1fr))",
                 li {
-                    div { class: item,
+                    div { class: &cls.item as &str,
                         span { color: "var(--overlay0)", "github" }
                         a {
-                            class: underlined,
+                            class: &cls.underlined as &str,
                             href: "https://github.com/mariesavch",
                             "mariesavch"
                         }
@@ -207,10 +196,10 @@ fn App() -> Element {
                 }
 
                 li {
-                    div { class: item,
+                    div { class: &cls.item as &str,
                         span { color: "var(--overlay0)", "email" }
                         a {
-                            class: underlined,
+                            class: &cls.underlined as &str,
                             href: "mailto:mariesavch@icloud.com",
                             "mariesavch@icloud.com"
                         }
@@ -219,65 +208,65 @@ fn App() -> Element {
             }
             h2 { font_size: "20px", margin_bottom: "8px", margin_top: "32px", "projects" }
             ul {
-                class: animated_list,
+                class: &cls.animated_list as &str,
                 all: "unset",
                 display: "grid",
                 grid_template_columns: "repeat(auto-fit, minmax(300px, 1fr))",
                 li {
-                    div { class: item,
+                    div { class: &cls.item as &str,
                         span { color: "var(--overlay0)", "view your weather" }
                         a {
-                            class: underlined,
+                            class: &cls.underlined as &str,
                             href: "https://wtrs.vercel.app",
                             "weather"
                         }
                     }
                 }
                 li {
-                    div { class: item,
+                    div { class: &cls.item as &str,
                         span { color: "var(--overlay0)", "simple todo app" }
                         a {
-                            class: underlined,
+                            class: &cls.underlined as &str,
                             href: "https://tdwr.vercel.app",
                             "todo"
                         }
                     }
                 }
                 li {
-                    div { class: item,
+                    div { class: &cls.item as &str,
                         span { color: "var(--overlay0)", "information about countries" }
                         a {
-                            class: underlined,
+                            class: &cls.underlined as &str,
                             href: "https://cntrn.vercel.app",
                             "countryinfo"
                         }
                     }
                 }
                 li {
-                    div { class: item,
+                    div { class: &cls.item as &str,
                         span { color: "var(--overlay0)", "cli information about countries" }
                         a {
-                            class: underlined,
+                            class: &cls.underlined as &str,
                             href: "https://github.com/mariesavch/countryfetch-rs",
                             "countryfetch-rs"
                         }
                     }
                 }
                 li {
-                    div { class: item,
+                    div { class: &cls.item as &str,
                         span { color: "var(--overlay0)", "information about ip's" }
                         a {
-                            class: underlined,
+                            class: &cls.underlined as &str,
                             href: "https://ipinf.vercel.app",
                             "ipinfo"
                         }
                     }
                 }
                 li {
-                    div { class: item,
+                    div { class: &cls.item as &str,
                         span { color: "var(--overlay0)", "track postal shipments" }
                         a {
-                            class: underlined,
+                            class: &cls.underlined as &str,
                             href: "https://pstr.vercel.app",
                             "tracking"
                         }
